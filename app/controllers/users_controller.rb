8b1678e6ac81421859c296198e0ccb4a872dcdd7
Charles Williams <cw.prime@gmail.com>
2012-05-09 21:51:16 -0400

    7 steps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,66 +5,61 @@ class UsersController < ApplicationController
 
  
  
- def index
-   @users = User.paginate(:page => params[:page])
-   @title = "All users"
- end
-def show
-    @user = User.find(params[:id])
-    @title = @user.name 
-  end
-
-def new
-    @user = User.new
-    @title = "Sign up"
-   
+    def index
+       @users = User.paginate(:page => params[:page])
+       @title = "All users"
+     end
+    def show
+        @user = User.find(params[:id])
+        @title = @user.name 
       end
-  
-  def create     
-    @user = User.new(params[:user])
-   if @user.save
-     sign_in @user    
-      redirect_to @user, :flash => { :success => "Welcome to the Sample App!"}
-    else
-    @title ="Sign up"
-    render 'new'
-  end
-  end
-  def edit    
-    
-    @title = "Edit User"
-  end
-  
-  def update
-   
-     if @user.update_attributes(params[:user])       
-       redirect_to @user, :flash => { :success => "Profile updated."}
-     else
+    def new
+        @user = User.new
+        @title = "Sign up"
        
+          end
+    def create     
+        @user = User.new(params[:user])
+       if @user.save
+         sign_in @user    
+          redirect_to @user, :flash => { :success => "Welcome to the Sample App!"}
+        else
+        @title ="Sign up"
+        render 'new'
+      end
+      end
+    def edit    
+        
         @title = "Edit User"
-    render 'edit'
+      end
+    def update
+       
+         if @user.update_attributes(params[:user])       
+           redirect_to @user, :flash => { :success => "Profile updated."}
+         else
+           
+            @title = "Edit User"
+        render 'edit'
+           end
        end
-   end
-   
-   def destroy
-     User.find(params[:id]).destroy
-     redirect_to users_path, :flash => { :success => "User destroyed."}
-   end
-   private 
-   
-    def authenticate
-    deny_access unless signed_in?    
-   end
-   
-   def correct_user
-     @user = User.find(params[:id])
-     redirect_to(root_path) unless current_user?(@user)
-     
-   end
-   def admin_user
-     user = User.find(params[:id])
-     redirect_to(root_path) if !current_user.admin? || current_user?(user)
-   end
-  
+    def destroy
+         User.find(params[:id]).destroy
+         redirect_to users_path, :flash => { :success => "User destroyed."}
+       end
+       private 
+       
+       def authenticate
+        deny_access unless signed_in?    
+       end
+       def correct_user
+         @user = User.find(params[:id])
+         redirect_to(root_path) unless current_user?(@user)
+         
+       end
+       def admin_user
+         user = User.find(params[:id])
+         redirect_to(root_path) if !current_user.admin? || current_user?(user)
+       end
+      
 end
 
